311c99e88b89a85df55252c96ce3c211f47a09fe
FrankYouze <frankyouze22@gmail.com>
2024-06-10 15:05:58 +0300

    10th june

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,47 +3,70 @@ import 'package:foody/pages/auth_page.dart';
 import 'package:foody/pages/client_page.dart';
 import 'package:foody/pages/login_page.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-backgroundColor: Colors.green[100],
-appBar: AppBar(title: Text("CafeApp",style: TextStyle(color: Colors.white),),backgroundColor: Colors.green,),
-
-
-body: Stack(
-  children :[ 
-    Center(
-      child: Container(
-        width: double.infinity,
-        height: 600,
-        child: Image(image: AssetImage("assets/zege.jpeg"),fit: BoxFit.cover,),),
-    ),
-    
-    Center(child: Container(height: 300,child: Column(children: [
-  ElevatedButton(
-    style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.green)),
-    onPressed: (){
-  
-      Navigator.push(context, MaterialPageRoute(builder: (context)=> ClientPage()));
-    }, child: Text("Client")),  SizedBox(height: 20,),
-  ElevatedButton(
-    style: ButtonStyle(backgroundColor: WidgetStateProperty.all(Colors.green)),
-    onPressed: (){
-  Navigator.push(context, MaterialPageRoute(builder: (context)=> AuthPage()));
-  
-    }, child: Text("Admin")),
-  
-    
-  
-  
-  ],),),),]
-),
-
-
+      backgroundColor: Colors.green[100],
+      appBar: AppBar(
+        title: Text(
+          "CafeApp",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.green,
+      ),
+      body: 
+      
+        SafeArea(
+          child: Container(
+            height: double.infinity,
+            child: Column(
+              children: [
+        
+        
+        Text("CAFERERIA",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                Container(
+            width: double.infinity,
+            height: 400,
+            child: Image(
+              image: AssetImage("assets/zege.jpeg"),
+              //fit: BoxFit.cover,
+            ),
+          ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.green)),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ClientPage()));
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 70,
+                      child: Center(child: Text("Client",style: TextStyle(color: Colors.white,fontSize: 20))))),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: WidgetStateProperty.all(Colors.green)),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => AuthPage()));
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 70,
+                      child: Center(child: Text("Admin",style: TextStyle(color: Colors.white,fontSize: 20),)))),
+              ],
+            ),
+          ),
+        ),
+     
     );
   }
 }
